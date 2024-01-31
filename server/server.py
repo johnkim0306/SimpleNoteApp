4037c667728d80e46dcd2377cf2523c6c7552f3a
John Kim <mkcokr2176@gmail.com>
2024-01-30 20:52:09 -0800

    Update crud operation update part

--- server/server.py
+++ server/server.py
@@ -1,11 +1,18 @@
-from flask import Flask # Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+members_data = ["member1", "member2", "member3"]
+
 @app.route('/members')
-def members():
-    return {"members": ["member1", "member2", "member3"]}
-           
+def get_members():
+    return jsonify({"members": members_data})
+
+@app.route('/add-member', methods=['POST'])
+def add_member():
+    new_member = "new_member"
+    members_data.append(new_member)
+    return jsonify({"members": members_data})
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
