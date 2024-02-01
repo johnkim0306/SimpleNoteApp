e696af5647116f2ab8f092aa239d6d63849a1a2f
John Kim <mkcokr2176@gmail.com>
2024-01-31 21:32:46 -0800

    Crud Delete Implemented

--- server/server.py
+++ server/server.py
@@ -10,8 +10,16 @@ def get_members():
 
 @app.route('/add-member', methods=['POST'])
 def add_member():
-    new_member = "new_member"
+    new_member = "new_member"  # Replace this with the logic to get the new member data
     members_data.append(new_member)
+    return jsonify({"members": members_data})
+
+@app.route('/delete-member', methods=['POST'])
+def delete_member():
+    # You can replace the logic to get the member to delete
+    member_to_delete = "new_member"
+    if member_to_delete in members_data:
+        members_data.remove(member_to_delete)
     return jsonify({"members": members_data})
 
 if __name__ == "__main__":
